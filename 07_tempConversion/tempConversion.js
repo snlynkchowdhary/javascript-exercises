74598ca79702f6a774d863ba53bd430e83ccdd0f
snlynkchowdhary <snlynkchowdhary@gmail.com>
2022-03-24 17:23:14 +0530

    Complete 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (tempf) {
+  const tempc = ((tempf - 32) * 5) / 9;
+  return Math.round(tempc * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (tempc) {
+  const tempf = (tempc * 9) / 5 + 32;
+  return Math.round(tempf * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
