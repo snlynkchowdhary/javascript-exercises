cc7fbbf1dd5dcc70bb799d735d0ef53f683ba01a
snlynkchowdhary <snlynkchowdhary@gmail.com>
2022-03-24 13:54:28 +0530

    Complete 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  let newStr = "";
+  if (times < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < times; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
